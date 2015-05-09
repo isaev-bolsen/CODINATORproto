95ed3efb0cf60505c082f8dd2d056d8feab1e114
drBolsen <al_mamistov@mail.ru>
2015-05-09 13:14:14 +0300

    Basic

---
+++ CODINATOR/ClassBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom;
+
+namespace CODINATOR
+    {
+    class ClassBuilder
+        {
+        private CodeCompileUnit ResultingSc;
+        private CodeNamespace Namespace;
+        private CodeTypeDeclaration Class;
+
+        public ClassBuilder(string NamespaceName, string className)
+            {
+            ResultingSc = new CodeCompileUnit();
+            Namespace = new CodeNamespace(NamespaceName);
+            Namespace.Imports.Add(new CodeNamespaceImport("System"));
+            ResultingSc.Namespaces.Add(Namespace);
+            Class = new CodeTypeDeclaration(className);
+            Namespace.Types.Add(Class);
+            }
+
+        public void addField(Type fieldType, string name)
+            {
+            CodeMemberField Field = new CodeMemberField(fieldType, name);
+            Class.Members.Add(Field);
+            }
+
+        public CodeCompileUnit GetResultingCompileUnit()
+            {
+            return ResultingSc;
+            }
+        }
+    }
